9c6ac9496e56ea09f537bd87cd33170e43e9691f
pibragimov <pibragimov@whoosh.bike>
2025-01-12 22:44:10 +0300

    add module 3 lessons 1-3

---
+++ module3/module3_lesson3_step8.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_registration1():
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "[required].first")
+    input1.send_keys("Firstname")
+    input2 = browser.find_element(By.CSS_SELECTOR, "[required].second")
+    input2.send_keys("Lastname")
+    input3 = browser.find_element(By.CSS_SELECTOR, "[required].third")
+    input3.send_keys("email@gmail.com")
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    time.sleep(10)
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайтаx
+    assert "Congratulations! You have successfully registered!" == welcome_text, "Something went wrong..."
+
+
+def test_registration2():
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "[required].first")
+    input1.send_keys("Firstname")
+    input2 = browser.find_element(By.CSS_SELECTOR, "[required].second")
+    input2.send_keys("Lastname")
+    input3 = browser.find_element(By.CSS_SELECTOR, "[required].third")
+    input3.send_keys("email@gmail.com")
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    time.sleep(10)
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайтаx
+    assert "Congratulations! You have successfully registered!" == welcome_text, "Something went wrong..."
+
+
+if __name__ == "__main__":
+    test_registration1()
+    test_registration2()
